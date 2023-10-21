07ca894c450407e43949feb1fefb8ef53a2a1286
Khairul-Ikhwan <khairul.ikhwan00@gmail.com>
2023-10-22 03:13:33 +0800

    Update package.json and server.js

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
-import express from "express"
-const app = express()
-const port = 3000
+import express from "express";
+import path from 'path';
+import logger from 'morgan';
+import favicon from 'serve-favicon';
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const __filename = new URL(import.meta.url).pathname;
+const __dirname = path.dirname(__filename);
+const app = express();
+const port = 3000;
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(favicon(path.join(__dirname, 'dist', 'vite.svg')));
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// Serve the index.html for all routes (client-side routing)
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Example app listening on port ${port}`);
+});
