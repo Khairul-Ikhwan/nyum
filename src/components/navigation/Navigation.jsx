957423e7025c24cb821ed8f516497c8baeb75049
Khairul-Ikhwan <khairul.ikhwan00@gmail.com>
2023-10-23 04:29:01 +0800

    Update navigation

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -3,11 +3,16 @@ export default function Navigation() {
     <div className="navigation-menu">
       <ul>
         <li>Home</li>
+        <li>FAQ</li>
         <li>About Us</li>
         <li>Contact Us</li>
         <li>Development Roadmap</li>
-        <li>FAQ</li>
       </ul>
+      {/* <div className="sub-menu">
+        <p>Settings</p>
+        <p>Account</p>
+        <p>Login</p>
+      </div> */}
     </div>
   );
 }
