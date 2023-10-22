342e8ff8b8dbf370cb99dc9521ae60a139c41e22
Khairul-Ikhwan <khairul.ikhwan00@gmail.com>
2023-10-23 03:52:50 +0800

    Update Navigation
    It slides now

---
+++ src/components/navigation/Navigation.jsx
@@ -0,0 +1,13 @@
+export default function Navigation() {
+  return (
+    <div className="navigation-menu">
+      <ul>
+        <li>Home</li>
+        <li>About Us</li>
+        <li>Contact Us</li>
+        <li>Development Roadmap</li>
+        <li>FAQ</li>
+      </ul>
+    </div>
+  );
+}
--- src/layouts/PageLayout.jsx
+++ src/layouts/PageLayout.jsx
@@ -1,4 +1,13 @@
+import { useState } from "react";
+import Navigation from "../components/navigation/Navigation";
+
 export default function PageLayout({ children }) {
+  const [navigationVisible, setNavigationVisible] = useState(false);
+
+  const toggleNavigation = () => {
+    setNavigationVisible(!navigationVisible);
+  };
+
   return (
     <div className="page-wrapper">
       <header
@@ -13,10 +22,12 @@ export default function PageLayout({ children }) {
         <div>
           <p style={{ fontWeight: 700, fontSize: "var(--logo-text)" }}>Nyum</p>
         </div>
-        <div>
+        <div onClick={toggleNavigation} style={{ cursor: "pointer" }}>
           <p>menu</p>
         </div>
       </header>
+      {navigationVisible && <Navigation />}
+
       <div className="children-wrapper">{children}</div>
       <footer style={{ color: "white" }}>
         <div>
