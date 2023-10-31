0f078d18810ef51498a88e7d8731be7c26d3aa2f
Khairul-Ikhwan <khairul.ikhwan00@gmail.com>
2023-11-01 01:57:53 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,12 @@ import StoreFront from "./pages/storeFront/StoreFront";
 
 function App() {
   return (
-    <>
+    <main className="App">
       <Routes>
         <Route path="/*" element={<HomePage />} />
         <Route path="/store/:id" element={<StoreFront />} />
       </Routes>
-    </>
+    </main>
   );
 }
 
