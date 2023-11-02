35d425d08cce91e08964705989241b86bdaa8fd9
Khairul-Ikhwan <khairul.ikhwan00@gmail.com>
2023-11-03 05:27:33 +0800

    Update ATC

--- src/pages/storeFront/AddToCard.jsx
+++ src/pages/storeFront/AddToCard.jsx
@@ -1,10 +1,24 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./storeFront.css";
 import SectionContainer from "../../components/heros/SectionContainer";
 
+function QtySelector({ quantity, onQuantityChange }) {
+  return (
+    <div className="qty-selector">
+      <button onClick={() => onQuantityChange(quantity - 1)}> - </button>
+      <input
+        type="number"
+        value={quantity}
+        onChange={(event) => onQuantityChange(parseInt(event.target.value))}
+        min="0"
+      />
+      <button onClick={() => onQuantityChange(quantity + 1)}> + </button>
+    </div>
+  );
+}
+
 export default function AddToCart({ show, hideModal, merchant, product }) {
   useEffect(() => {
-    console.log(product);
     if (show) {
       document.body.classList.add("disable-scroll");
     } else {
@@ -16,41 +30,29 @@ export default function AddToCart({ show, hideModal, merchant, product }) {
     };
   }, [show]);
 
-  return (
-    <div className={`modal ${show ? "active" : ""}`}>
-      <div
-        className="modal-header"
-        style={{
-          display: "flex",
-          flexDirection: "row",
-          width: "inherit",
-          placeItems: "center",
-          justifyContent: "space-between",
-        }}
-      >
-        <span>
-          <h3>Add To Cart</h3>
-          <p
-            style={{
-              fontWeight: "100",
-              fontSize: ".75rem",
-              fontStyle: "italic",
-            }}
-          >
-            {merchant.name}
-          </p>
-        </span>
+  // State to manage quantity
+  const [quantity, setQuantity] = useState(0);
 
-        <button className="header-btn" onClick={hideModal}>
-          X
-        </button>
-      </div>
+  // Function to handle quantity change
+  const handleQuantityChange = (newQuantity) => {
+    if (!isNaN(newQuantity) && newQuantity >= 0) {
+      setQuantity(newQuantity);
+    }
+  };
 
-      <div className="modal-container">
-        <SectionContainer>
-          <form>
-            <div>
-              <h4>{product.productName}</h4>
+  const renderForm = () => {
+    if (product.variants) {
+      return (
+        <>
+          <h4>{product.productName}</h4>
+          <div className="variant-selector">
+            <section
+              style={{
+                display: "flex",
+                flexDirection: "column",
+              }}
+            >
+              <label>Please select an option</label>
               <select>
                 {Object.keys(product.variants).map((variantKey) => (
                   <option key={variantKey} value={variantKey}>
@@ -58,10 +60,52 @@ export default function AddToCart({ show, hideModal, merchant, product }) {
                   </option>
                 ))}
               </select>
-            </div>
+            </section>
+
+            <QtySelector
+              quantity={quantity}
+              onQuantityChange={handleQuantityChange}
+            />
+          </div>
+        </>
+      );
+    } else {
+      return (
+        <>
+          <div className="variant-selector">
+            <h4>{product.productName}</h4>
+            <QtySelector
+              quantity={quantity}
+              onQuantityChange={handleQuantityChange}
+            />
+          </div>
+        </>
+      );
+    }
+  };
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <div className={`modal ${show ? "active" : ""}`}>
+      <div className="modal-header">
+        <span>
+          <h3>Add To Cart</h3>
+          <p className="merchant-info">{merchant.name}</p>
+        </span>
+        <button className="header-btn" onClick={hideModal}>
+          X
+        </button>
+      </div>
+      <div className="modal-container">
+        <SectionContainer>
+          <form onSubmit={handleFormSubmit}>
+            <div>{renderForm()}</div>
           </form>
         </SectionContainer>
-        <button>Add another</button>
+        <button>Add To Cart</button>
       </div>
     </div>
   );
