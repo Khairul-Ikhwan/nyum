a9ff9cad5ebe2098efedff8bd1e85e1877a7a489
Khairul-Ikhwan <khairul.ikhwan00@gmail.com>
2023-11-10 18:31:41 +0800

    Update Review.css

--- src/components/store/Reviews.jsx
+++ src/components/store/Reviews.jsx
@@ -7,8 +7,18 @@ export default function Reviews({ reviews }) {
       {reviews.map((review, reviewId) => (
         <div key={reviewId}>
           <p>{review.reviewText}</p>
-          <p>by {review.reviewer}</p>
-          <p>{calculateTimeElapsed(review.reviewDate)}</p>
+          <p
+            style={{
+              fontSize: ".9rem",
+              fontWeight: "300",
+              fontStyle: "italic",
+            }}
+          >
+            by {review.reviewer}
+          </p>
+          <p style={{ fontWeight: "100", fontSize: ".85rem" }}>
+            {calculateTimeElapsed(review.reviewDate)}
+          </p>
         </div>
       ))}
     </SectionBox>
